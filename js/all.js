28f056910f769ba517fd440c52852307283d8aca
Kai（カイ） <asdf23146@gmail.com>
2022-06-10 10:13:23 +0800

    Finally

--- js/all.js
+++ js/all.js
@@ -54,28 +54,28 @@ $(document).ready(function() {
 });
 
 // Initialize Swiper
-// var swiper = new Swiper(".mySwiper", {
-//     slidesPerView: 3,
-//     spaceBetween: 16,
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 16,
 
-//     /* 斷點設定 */
-//     breakpoints: {
-//       /* 螢幕寬度大於等於 992px 時切換為 3 欄 */
-//       992: {
-//         slidesPerView: 3,
-//       },
-//       /* 螢幕寬度大於等於 768px 時切換為 2 欄 */
-//       768: {
-//         slidesPerView: 2,
-//       },
-//       /* 更小時都顯示 1 欄 */
-//       0: {
-//         slidesPerView: 1,
-//       },
-//     },
+    /* 斷點設定 */
+    breakpoints: {
+      /* 螢幕寬度大於等於 992px 時切換為 3 欄 */
+      992: {
+        slidesPerView: 3,
+      },
+      /* 螢幕寬度大於等於 768px 時切換為 2 欄 */
+      768: {
+        slidesPerView: 2,
+      },
+      /* 更小時都顯示 1 欄 */
+      0: {
+        slidesPerView: 1,
+      },
+    },
 
-//     pagination: {
-//       el: ".swiper-pagination",
-//       clickable: true,
-//     },
-// });
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
